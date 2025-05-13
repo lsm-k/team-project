3d80bf28bb489d2abeb251465dcb0306c4946ef7
lsm-k <tmdals9482@gmail.com>
2025-05-14 01:01:47 +0900

    code update

--- Code_test/test.py
+++ Code_test/test.py
@@ -1,6 +1,8 @@
 import sqlite3
+import os
 
-connection = sqlite3.connect("test.db")
+db_path = os.path.join(os.path.dirname(__file__), "test.db")
+connection = sqlite3.connect(db_path)
 curser = connection.cursor()
 
 curser.execute("""
@@ -8,8 +10,14 @@ curser.execute("""
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         username TEXT NOT NULL,
         password TEXT NOT NULL,
-        email TEXT NOT NULL,
+        email TEXT NOT NULL
+    )
 """)
+
+# 테이블 목록 출력
+curser.execute("SELECT name FROM sqlite_master WHERE type='table';")
+tables = curser.fetchall()
+print("생성된 테이블블", tables)
 
 connection.commit()
 connection.close()
