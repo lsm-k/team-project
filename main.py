d388162808747362c7f7f9fc7901cc2ac2949ef9
lsm-k <63622172+lsm-k@users.noreply.github.com>
2025-05-22 03:15:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from interface import interface_main as i_m
+<<<<<<< HEAD
 from database.cold_storage import db as cs_db
 
 # d_main = d_m.Database()
@@ -7,6 +8,12 @@ from database.cold_storage import db as cs_db
 # d_main.data_edit_amount('사과', 5)
 cold_storage = cs_db.Database()
 cold_storage.setting_table()
+=======
+from database import database_main as d_m
+
+# main_window = i_m.Mainwindow()
+# main_window.mainrun()
+>>>>>>> parent of 261cda4 (change to pyside6 and migrate previous changes)
 
 d_main = d_m.Database()
 
