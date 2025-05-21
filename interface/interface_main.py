9461f5ee4317bd1868fcd23ca5cd5c96f5415ade
lsm-k <63622172+lsm-k@users.noreply.github.com>
2025-05-22 03:22:36 +0900

    g

--- interface/interface_main.py
+++ interface/interface_main.py
@@ -1,12 +1,23 @@
 import sys
 import os
-from PyQt6 import QtWidgets, uic
+from PySide6.QtWidgets import QApplication
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtCore import QFile
 
-class Mainwindow():
+class Mainwindow:
     @classmethod
     def mainrun(cls):
-        app = QtWidgets.QApplication(sys.argv)
-        ui_file = os.path.join(os.path.dirname(__file__), "material_stat.ui")
-        window = uic.loadUi(ui_file)
+        app = QApplication(sys.argv)
+
+        ui_file = QFile(
+            os.path.join(os.path.dirname(__file__), "material_stat.ui")
+        )
+        ui_file.open(QFile.ReadOnly)
+        
+        loader = QUiLoader()
+        window = loader.load(ui_file)
+        ui_file.close()
+
         window.show()
+
         sys.exit(app.exec())
