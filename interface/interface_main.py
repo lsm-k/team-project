89daf5bc06e7e6c8a3bac0b8ca66d60619b02333
lsm-k <63622172+lsm-k@users.noreply.github.com>
2025-05-22 02:54:59 +0900

    mistake

--- interface/interface_main.py
+++ interface/interface_main.py
@@ -1,23 +1,12 @@
 import sys
 import os
-from PySide6.QtWidgets import QApplication
-from PySide6.QtUiTools import QUiLoader
-from PySide6.QtCore import QFile
+from PyQt6 import QtWidgets, uic
 
-class Mainwindow:
+class Mainwindow():
     @classmethod
     def mainrun(cls):
-        app = QApplication(sys.argv)
-
-        ui_file = QFile(
-            os.path.join(os.path.dirname(__file__), "material_stat.ui")
-        )
-        ui_file.open(QFile.ReadOnly)
-        
-        loader = QUiLoader()
-        window = loader.load(ui_file)
-        ui_file.close()
-
+        app = QtWidgets.QApplication(sys.argv)
+        ui_file = os.path.join(os.path.dirname(__file__), "material_stat.ui")
+        window = uic.loadUi(ui_file)
         window.show()
-
         sys.exit(app.exec())
