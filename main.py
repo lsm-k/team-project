1386258a87411ded47e2e49beac0ebaff930536c
lsm-k <63622172+lsm-k@users.noreply.github.com>
2025-05-22 02:55:21 +0900

    ?

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from interface import interface_main as i_m
-from database import database_main as d_m
-
-<<<<<<< HEAD
-main_window = i_m.Mainwindow()
-main_window.mainrun()
+from interface import interface_main as ui_root
+from database.cold_storage import db as cs_db
 
 # d_main = d_m.Database()
 
@@ -11,12 +7,6 @@ main_window.mainrun()
 # d_main.data_edit_amount('사과', 5)
 cold_storage = cs_db.Database()
 cold_storage.setting_table()
-=======
-# main_window = i_m.Mainwindow()
-# main_window.mainrun()
->>>>>>> parent of 261cda4 (change to pyside6 and migrate previous changes)
 
-d_main = d_m.Database()
-
-d_main.setting_table()
-d_main.data_edit_amount('사과', 5)
+main_window = ui_root.Mainwindow()
+main_window.mainrun()
