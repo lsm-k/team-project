36ef0f6b454d3849a59d822ac0a4ad5ec697c042
Raenbeo <akwdkk39@gmail.com>
2025-05-26 23:51:57 +0900

    fixup! add favorite_ref

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from interface import interface_main as ui_root
 from database.cold_storage import db as cs_db
+from database.favorite_ref import db as fr_db
 
 cold_storage = cs_db.Database()
 cold_storage.setting_table()
+
+fr_db.Database.setting_table()
 
 main_window = ui_root.Mainwindow()
 main_window.show()
